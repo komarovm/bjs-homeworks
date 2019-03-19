090dd675c88d54bf250ca9225cac06fade3f85dc
mikhailkomarov <michaelkomarov1@gmail.com>
2019-03-19 13:24:35 +0300

    first commit in variables/main.js

---
+++ variables/main.js
@@ -0,0 +1,35 @@
+// Задание №1 
+
+let x, y, z;
+
+x = 2;
+
+y = 22;
+
+z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
+
+// Задание №2
+
+let algebra, geography, physics;
+
+algebra = 5;
+
+geography = 4;
+
+physics = 4;
+
+result = (algebra + geography + physics) / 3;
+
+console.log(result);
+
+// Задание №3
+
+let name = 'Михаил';
+
+let message  = `Привет, мир! Меня зовут ${name}.`;
+
+console.log(message);
